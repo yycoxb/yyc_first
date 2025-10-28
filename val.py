143246f61d6cf5ced2b56baffeb10efeed0d791c
yycoxb <958721367@qq.com>
2025-10-28 22:40:27 +0800

    修改 file

--- val.py
+++ val.py
@@ -2,7 +2,7 @@ import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
-# BILIBILI UP 魔傀面具
+
 # 验证参数官方详解链接：https://docs.ultralytics.com/modes/val/#usage-examples:~:text=of%20each%20category-,Arguments%20for%20YOLO%20Model%20Validation,-When%20validating%20YOLO
 
 # 精度小数点保留位数修改问题可看<使用说明.md>下方的<YOLOV8源码常见疑问解答小课堂>第五点
